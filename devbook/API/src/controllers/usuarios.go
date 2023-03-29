925e7652f6b9ee05d61ed9387b04a1b54ce6eecf
Washington Alencar <87153926+WashiiUkeda@users.noreply.github.com>
2023-03-29 11:52:34 -0300

    Add files via upload

---
+++ devbook/API/src/controllers/usuarios.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// CriarUsuario insere um usuário no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = usuario.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuario.ID, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, usuario)
+}
+
+// BuscarUsuarios busca todos os usuários salvos no banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	nomeOuNick := strings.ToLower(r.URL.Query().Get("usuario"))
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarios, erro := repositorio.Buscar(nomeOuNick)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, usuarios)
+}
+
+// BuscarUsuario busca um usuário salvo no banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando usuário"))
+}
+
+// AtualizarUsuario altera as informações de um usuário no banco de dados
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando usuário"))
+}
+
+// DeletarUsuario remove um usuário do banco de dados
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Removendo usuário"))
+}
