796c8126ba15bcc796921fb62b4d6232b6bfc017
washington alencar <washington.alencar@g.globo>
2023-06-05 23:50:13 -0300

    feat: cria rota Buscar Publicacao po ID

--- devbook/API/src/controllers/publicacoes.go
+++ devbook/API/src/controllers/publicacoes.go
@@ -9,6 +9,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // CriarPublicacoes adiciona uma nova publicação no banco de dados
@@ -56,12 +59,33 @@ func CriarPublicacoes(w http.ResponseWriter, r *http.Request) {
 
 // BuscarPublicacoes traz as publicações que apareceriam no feed do usuário
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
-
+	
 }
 
 // BuscarPublicacao busca uma única publicação
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"],10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacao)
 }
 
 // AtualizarPublicacao altera os dados de uma publicação
